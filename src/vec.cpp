e4ed707a7f9e96622f10b1bf797aa1cbb7c29162
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2023-06-06 16:48:58 +0200

    Refactor vec(2|3)f
    ~ender

--- src/vec.cpp
+++ src/vec.cpp
@@ -1,7 +1,7 @@
-#include "vec.h"
-
-#include <cmath>
 #include <sstream>
+#include <xmmintrin.h>
+
+#include "vec.hpp"
 
 // V     V  EEEEEEE    CCC      222    FFFFFFF
 // V     V  E         CC CC    2   22  F
@@ -14,115 +14,80 @@
 //    V     EEEEEEE    CCC    2222222  F
 
 
-// Quake <3
-
-inline float fast_inv_sqrt(float x) // NOTE: Firm: outdated by rsqrtss
-{
-    union {float f; unsigned long ul;} y;
-    y.f = x;
-    y.ul = (0xBE6EB50CUL - y.ul) >> 1;
-    y.f = 0.5f * y.f * (3.0f - x * y.f * y.f);
-    return y.f;
+inline float fast_inv_sqrt( float x ) {
+	// use the rsqrtss x86 instruction to calc it, its faster
+	return _mm_cvtss_f32( _mm_rsqrt_ss( _mm_set_ps1( x ) ) );
 }
 
-inline float fast_sqrt(float x)
-{
-    union {float f; unsigned long ul;} y;
-    y.f = x;
-    y.ul = (0xBE6EB50CUL - y.ul) >> 1;
-    y.f = 0.5f * y.f * (3.0f - x * y.f * y.f);
-    return x * y.f;
+inline float fast_sqrt( float x ) {
+	return _mm_cvtss_f32( _mm_sqrt_ss( _mm_set_ps1( x ) ) );
 }
 
-vec2f vec2f::operator+(const vec2f &rhs) const
-{
-    return vec2f(x + rhs.x,
-                y + rhs.y);
+vec2f vec2f::operator+( const vec2f& rhs ) const {
+	return vec2f{ x + rhs.x, y + rhs.y };
 }
 
-vec2f vec2f::operator-(const vec2f &rhs) const
-{
-    return vec2f(x - rhs.x,
-                y - rhs.y);
+vec2f vec2f::operator-( const vec2f& rhs ) const {
+	return vec2f{ x - rhs.x, y - rhs.y };
 }
 
-vec2f vec2f::operator*(float rhs) const
-{
-    return vec2f(x * rhs,
-                y * rhs);
+vec2f vec2f::operator*( float rhs ) const {
+	return vec2f{ x * rhs, y * rhs };
 }
 
-vec2f vec2f::operator/(float rhs) const
-{
-    return vec2f(x / rhs,
-                y / rhs);
+vec2f vec2f::operator/( float rhs ) const {
+	return vec2f{ x / rhs, y / rhs };
 }
 
-vec2f& vec2f::operator+=(const vec2f &rhs)
-{
-    x += rhs.x;
-    y += rhs.y;
-    return *this;
+vec2f& vec2f::operator+=( const vec2f& rhs ) {
+	x += rhs.x;
+	y += rhs.y;
+	return *this;
 }
 
-vec2f& vec2f::operator-=(const vec2f &rhs)
-{
-    x -= rhs.x;
-    y -= rhs.y;
-    return *this;
+vec2f& vec2f::operator-=( const vec2f& rhs ) {
+	x -= rhs.x;
+	y -= rhs.y;
+	return *this;
 }
 
-vec2f& vec2f::operator*=(float rhs)
-{
-    x *= rhs;
-    y *= rhs;
-    return *this;
+vec2f& vec2f::operator*=( float rhs ) {
+	x *= rhs;
+	y *= rhs;
+	return *this;
 }
 
-vec2f& vec2f::operator/=(float rhs)
-{
-    x /= rhs;
-    y /= rhs;
-    return *this;
+vec2f& vec2f::operator/=( float rhs ) {
+	x /= rhs;
+	y /= rhs;
+	return *this;
 }
 
-bool vec2f::operator==(const vec2f &rhs) const
-{
-    return x == rhs.x && y == rhs.y;
+bool vec2f::operator==( const vec2f& rhs ) const {
+	return x == rhs.x && y == rhs.y;
 }
 
-bool vec2f::operator<(const vec2f &rhs) const
-{
-    return x < rhs.x || (x == rhs.x && y < rhs.y);
+bool vec2f::operator<( const vec2f& rhs ) const {
+	return x < rhs.x || ( x == rhs.x && y < rhs.y );
 }
 
-float vec2f::dot(const vec2f &rhs) const
-{
-    return x * rhs.x + y * rhs.y;
+float vec2f::dot( const vec2f& rhs ) const {
+	return x * rhs.x + y * rhs.y;
 }
 
-float vec2f::length() const
-{
-    return sqrtf(x * x + y * y);
+float vec2f::length() const {
+	return fast_sqrt( x * x + y * y );
 }
 
-vec2f vec2f::normalise() const
-{
-    return *this * fast_inv_sqrt(x * x + y * y);
+vec2f vec2f::normalise() const {
+	return *this * fast_inv_sqrt( x * x + y * y );
 }
 
-std::string vec2f::toString()
-{
-    std::stringstream ss;
-    ss << "(" << x << ", " << y << ")";
-    return ss.str();
+std::string vec2f::toString() const {
+	std::stringstream ss;
+	ss << "(" << x << ", " << y << ")";
+	return ss.str();
 }
-
-/*vec2f::vec2f(float _x, float _y)
-{
-    x = _x;
-    y = _y;
-}*/
 
 // V     V  EEEEEEE    CCC      333    FFFFFFF
 // V     V  E         CC CC   33   33  F
@@ -135,96 +100,72 @@ std::string vec2f::toString()
 //    V     EEEEEEE    CCC      333    F
 
 
-
-vec3f vec3f::operator+(const vec3f &rhs) const
-{
-    return vec3f(x + rhs.x,
-                 y + rhs.y,
-                 z + rhs.z);
+vec3f vec3f::operator+( const vec3f& rhs ) const {
+	return vec3f{ x + rhs.x, y + rhs.y, z + rhs.z };
 }
 
-vec3f vec3f::operator-(const vec3f &rhs) const
-{
-    return vec3f(x - rhs.x,
-                 y - rhs.y,
-                 z - rhs.z);
+vec3f vec3f::operator-( const vec3f& rhs ) const {
+	return vec3f{ x - rhs.x, y - rhs.y, z - rhs.z };
 }
 
-vec3f vec3f::operator*(float rhs) const
-{
-    return vec3f(x * rhs,
-                 y * rhs,
-                 z * rhs);
+vec3f vec3f::operator*( float rhs ) const {
+	return vec3f{ x * rhs, y * rhs, z * rhs };
 }
 
-vec3f vec3f::operator/(float rhs) const
-{
-    return vec3f(x / rhs,
-                 y / rhs,
-                 z / rhs);
+vec3f vec3f::operator/( float rhs ) const {
+	return vec3f{ x / rhs, y / rhs, z / rhs };
 }
 
-vec3f& vec3f::operator+=(const vec3f &rhs)
-{
-    x += rhs.x;
-    y += rhs.y;
-    z += rhs.z;
-    return *this;
+vec3f& vec3f::operator+=( const vec3f& rhs ) {
+	x += rhs.x;
+	y += rhs.y;
+	z += rhs.z;
+	return *this;
 }
 
-vec3f& vec3f::operator-=(const vec3f &rhs)
-{
-    x -= rhs.x;
-    y -= rhs.y;
-    z -= rhs.z;
-    return *this;
+vec3f& vec3f::operator-=( const vec3f& rhs ) {
+	x -= rhs.x;
+	y -= rhs.y;
+	z -= rhs.z;
+	return *this;
 }
 
-vec3f& vec3f::operator*=(float rhs)
-{
-    x *= rhs;
-    y *= rhs;
-    z *= rhs;
-    return *this;
+vec3f& vec3f::operator*=( float rhs ) {
+	x *= rhs;
+	y *= rhs;
+	z *= rhs;
+	return *this;
 }
 
-vec3f& vec3f::operator/=(float rhs)
-{
-    x /= rhs;
-    y /= rhs;
-    y /= rhs;
-    return *this;
+vec3f& vec3f::operator/=( float rhs ) {
+	x /= rhs;
+	y /= rhs;
+	y /= rhs;
+	return *this;
 }
 
-bool vec3f::operator==(const vec3f &rhs) const
-{
-    return x == rhs.x && y == rhs.y && z == rhs.z;
+bool vec3f::operator==( const vec3f& rhs ) const {
+	return x == rhs.x && y == rhs.y && z == rhs.z;
 }
 
-bool vec3f::operator<(const vec3f &rhs) const
-{
-    return x < rhs.x || (x == rhs.x && (y < rhs.y || (y == rhs.y && z < rhs.z)));
+bool vec3f::operator<( const vec3f& rhs ) const {
+	return x < rhs.x || ( x == rhs.x && ( y < rhs.y || ( y == rhs.y && z < rhs.z ) ) );
 }
 
-float vec3f::dot(const vec3f &rhs) const
-{
-    return x * rhs.x + y * rhs.y + z * rhs.z;
+float vec3f::dot( const vec3f& rhs ) const {
+	return x * rhs.x + y * rhs.y + z * rhs.z;
 }
 
-float vec3f::length() const
-{
-    return sqrtf(x * x + y * y + z * z);
+float vec3f::length() const {
+	return fast_sqrt( x * x + y * y + z * z );
 }
 
-vec3f vec3f::normalise() const
-{
-    return *this * fast_inv_sqrt(x * x + y * y + z * z);
+vec3f vec3f::normalise() const {
+	return *this * fast_inv_sqrt( x * x + y * y + z * z );
 }
 
-std::string vec3f::toString()
-{
-    std::stringstream ss;
-    ss << "(" << x << ", " << y << ", " << z << ")";
-    return ss.str();
+std::string vec3f::toString() const {
+	std::stringstream ss;
+	ss << "(" << x << ", " << y << ", " << z << ")";
+	return ss.str();
 }
-
---
+++ src/vec.hpp
@@ -0,0 +1,56 @@
+#pragma once
+
+#include <string>
+
+
+struct vec2f {
+	float x;
+	float y;
+
+	vec2f operator+( const vec2f& rhs ) const;
+	vec2f operator-( const vec2f& rhs ) const;
+	vec2f operator*( float rhs ) const;
+	vec2f operator/( float rhs ) const;
+	vec2f& operator+=( const vec2f& rhs );
+	vec2f& operator-=( const vec2f& rhs );
+	vec2f& operator*=( float rhs );
+	vec2f& operator/=( float rhs );
+	bool operator==( const vec2f& rhs ) const;
+	bool operator<( const vec2f& rhs ) const;  // (lexicographic comparison only)
+	[[nodiscard]] float dot( const vec2f& rhs ) const;
+	[[nodiscard]] float length() const;
+	[[nodiscard]] vec2f normalise() const;
+	[[nodiscard]] std::string toString() const;
+
+	explicit inline vec2f( float _x = 0, float _y = 0 )  {
+		x = _x;
+		y = _y;
+	}
+};
+
+struct vec3f {
+	float x;
+	float y;
+	float z;
+
+	vec3f operator+( const vec3f& rhs ) const;
+	vec3f operator-( const vec3f& rhs ) const;
+	vec3f operator*( float rhs ) const;
+	vec3f operator/( float rhs ) const;
+	vec3f& operator+=( const vec3f& rhs );
+	vec3f& operator-=( const vec3f& rhs );
+	vec3f& operator*=( float rhs );
+	vec3f& operator/=( float rhs );
+	bool operator==( const vec3f& rhs ) const;
+	bool operator<( const vec3f& rhs ) const;  // (lexicographic comparison only)
+	[[nodiscard]] float dot( const vec3f& rhs ) const;
+	[[nodiscard]] float length() const;
+	[[nodiscard]] vec3f normalise() const;
+	[[nodiscard]] std::string toString() const;
+
+	explicit inline vec3f( float _x = 0, float _y = 0, float _z = 0 ) {
+		x = _x;
+		y = _y;
+		z = _z;
+	}
+};
