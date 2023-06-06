b056ad072e991109e1f93c7ad52a8995ec1e0d1b
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2023-06-06 15:15:13 +0200

    Hopefully more performant hex to color conversion
    ~ender

--- src/material.cpp
+++ src/material.cpp
@@ -1,25 +1,22 @@
 #include <iostream>
+#include <string>
+#include <RmlUi/Core/Log.h>
 
 #include "material.hpp"
 
-
-int hex2int( const std::string& str ) {
-	std::stringstream ss;
-	ss << std::hex << str;
-	int x;
-	ss >> x;
-	return x;
-}
 
 vec3f hex2Color( std::string str ) {  //  e.g. "#00FF00";
 	if ( str[ 0 ] == '#' )
 		str = str.substr( 1 );
 	if ( str.length() != 6 )
 		std::cout << "Error: badly formed hex color value!\n";
+
+	auto color = std::stoi( str, nullptr, 16 );
+
 	return {
-		static_cast<float>( hex2int( str.substr( 0, 2 ) ) ) / 255.f,
-		static_cast<float>( hex2int( str.substr( 2, 2 ) ) ) / 255.f,
-		static_cast<float>( hex2int( str.substr( 4, 2 ) ) ) / 255.f
+		static_cast<float>( ( color & 0xFF'00'00 ) >> 16 ) / 255,  // R
+		static_cast<float>( ( color & 0x00'FF'00 ) >>  8 ) / 255,  // G
+		static_cast<float>( ( color & 0x00'00'FF ) >>  0 ) / 255   // B
 	};
 }
 
@@ -29,5 +26,5 @@ Material::Material( const Json::Value& root ) {
 	this->strength = root.get( "strength", 1 ).asFloat() / this->mass * 1000;
 	this->color = hex2Color( ( root.isMember( "color" ) ? root[ "color" ] : root[ "colour" ] ).asString() );  // may as well account for English spelling...
 	this->isHull = root[ "isHull" ].asBool();
-	std::cout << "Adding new Material: \"" << this->name << "\" " << this->color.toString() << "\n";
+	Rml::Log::Message( Rml::Log::Type::LT_INFO, "Adding new Material: \"%s\" %s", this->name.c_str(), this->color.toString().c_str() );
 }
