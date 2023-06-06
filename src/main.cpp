2bd72d274bc651b0824d0148bc9a7119cf9fc0d5
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2023-06-06 16:47:51 +0200

    Check whether we're running in `/run`
    ~ender

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,17 @@
  * Copyright: Luke Wren (http://github.com/Wren6991)
  * License:
  **************************************************************/
+#include <filesystem>
+#include <iostream>
 
 #include "ShipSandbox.hpp"
 
 int main() {
+	// make sure we're running in `$REPO/run`
+	if ( std::filesystem::current_path().filename() != "run" ) {
+		std::cerr << "Game wasn't run in `$REPO/run` directory, aborting." << std::endl;
+		return 1;
+	}
 	shipSandbox = new ShipSandbox{};
 	return shipSandbox->mainLoop();
 }
