040adeff6f88c23cf08bc47288a1318c0a13e1e2
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2023-06-28 00:59:15 +0200

    Use KHR_debug
    ~ender

--- renderer2/src/renderer/debug.cpp
+++ renderer2/src/renderer/debug.cpp
@@ -25,12 +25,12 @@ namespace renderer::Debug {
 		lastMessage.id = id;
 		lastMessage.severity = severity;
 		lastMessage.text = new std::string( message, length );
+		spdlog::info( "Something: {}", std::string( message, length ) );
 	}
 
 	auto attach( const GLFWwindow* window ) -> void {
-		glEnable( GL_ARB_debug_output );
-		glEnable( GL_DEBUG_OUTPUT_SYNCHRONOUS_ARB );
-		glDebugMessageCallbackARB( logMessage, window );
+		glEnable( GL_KHR_debug );
+		glDebugMessageCallback( logMessage, window );
 	}
 
 	auto update( const char* file, const char* func, int line ) -> void {
@@ -38,15 +38,19 @@ namespace renderer::Debug {
 			return;
 
 		switch ( lastMessage.severity ) {
-			case GL_DEBUG_SEVERITY_HIGH_ARB:
+			case GL_DEBUG_SEVERITY_HIGH:
 				spdlog::error( "[OpenGL Debug HIGH] in `{}:{}` at line {}: {}", file, func, line, *lastMessage.text );
 				break;
-			case GL_DEBUG_SEVERITY_MEDIUM_ARB:
+			case GL_DEBUG_SEVERITY_MEDIUM:
 				spdlog::warn( "[OpenGL Debug MEDIUM] in `{}:{}` at line {}: {}", file, func, line, *lastMessage.text );
 				break;
-			case GL_DEBUG_SEVERITY_LOW_ARB:
+			case GL_DEBUG_SEVERITY_LOW:
 				spdlog::info( "[OpenGL Debug LOW] in `{}:{}` at line {}: {}", file, func, line, *lastMessage.text );
 				break;
+			case GL_DEBUG_SEVERITY_NOTIFICATION:
+				spdlog::error( "Invalid severity: {}", lastMessage.severity );
+				break;
 		}
+		lastMessage.text = nullptr;
 	}
 }// namespace renderer::Debug
